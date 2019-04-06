b4d1dcaa012c46783d40d0968b04419267743511
Lorenzo <massimilianilorenzo@gmail.com>
2019-04-06 13:37:05 +0200

    Merge branch 'master' of C:\Users\Lorenzo\IdeaProjects\MiniErlangBlockchain with conflicts.

--- MassimilianiVainigli-Node/mv.erl
+++ MassimilianiVainigli-Node/mv.erl
@@ -1,30 +1,30 @@
 %%%-------------------------------------------------------------------
 %%% @author Lorenzo Massimiliani, Lorenzo Vainigli
 %%%-------------------------------------------------------------------
+
 -module(mv).
--author("Lorenzo Massimiliani, Lorenzo Vainigli").
 -export([test/0, test_node/0]).
 
 sleep(N) -> receive after N*1000 -> ok end.
 
-%%%%%%%%%%%%%%%% Testing only, do not use! %%%%%%%%%%%%%%%%%%%%
 
-%% P = spawn(mv, test_node, []).
-%% P1 = spawn(mv, test_node, []).
-%% P ! {ping, P1, 0}.
+
+
 
 test_node() ->
   receive
     {ping, Sender, Ref} -> Sender ! {pong, Ref} ;
-    {pong, Ref} -> io:format("Pong!") ;
     {friends, _, Nodes} ->
       io:format("~p nodes discovered~n", [length(Nodes)])
-  after 300000 -> teacher_node ! {get_friends, self(), make_ref()}
+  after 3000 -> teacher_node ! {get_friends, self(), make_ref()}
   end,
   test_node ().
 
+
+
+
 test() ->
-  spawn(teacher_node, main, []), % teacher node
+  spawn(teacher_node,main,[]), % teacher_node
   T1 = spawn(fun test_node/0),
   spawn(fun test_node/0),
   spawn(fun test_node/0),
