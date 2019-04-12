c64f6f66dca32898f845c5fa9358cd03e28771da
Andrea269 <nash25032710>
2019-04-12 11:22:29 +0200

    Add function in test in FasanoPascali-Node directory

--- FasanoPascali-Node/test.erl
+++ FasanoPascali-Node/test.erl
@@ -1,5 +1,5 @@
 -module(test).
--export([test/0, start/0, iter/1]).
+-export([test/0, init/0, start/1, getNewFriends/2, ext3Friends/3]).
 
 % This is the teacher node, the one responsible
 % for letting new nodes in the blockchain.
@@ -24,55 +24,69 @@ watch(Main, Node) ->
 test() ->
   spawn(teacher_node, main, []),
   sleep(3),
-  spawn(test, start, []),
-  spawn(test, start, []),
-  spawn(test, start, []),
-  spawn(test, start, [])
-  .
+  spawn(test, init, []),
+  spawn(test, init, []),
+  spawn(test, init, []),
+  spawn(test, init, [])
+.
 
 init() ->
 %%      chiedo amici
   global:send(teacher_node, {get_friends, self(), make_ref()}),
-  start().
+  start([]).
 
-start() ->
+start(Friends) ->
   receive
-    {friends, _, ListFriends} ->
-      io:format("friend receive~n"),
-      List3Friends = ext3Friends(ListFriends, [self()]),
-      io:format("List3Friends~p~p      ~n", [self(),List3Friends]),
-      PID = spawn(test, iter, [List3Friends]),
-      [spawn(fun() -> watch(PID, X) end) || X <- List3Friends],
-      start();
+    {friends, Nonce, ListFriends} ->
+      io:format("friend receive ~p ~p~n", [self(),Nonce]),
+      NewFriends = ext3Friends(ListFriends, [self()] ++ Friends, Friends),
+      [spawn(fun() -> watch(self(), X) end) || X <- NewFriends],
+%%      io:format("List3Friends~p~p      ~n", [self(),NewFriends]),
+      NewListFriends = Friends ++ NewFriends,
+      spawn(test, getNewFriends, [NewListFriends, self()]),
+      start(NewListFriends);
     {ping, Sender, Ref} ->
-      Sender ! {pong, Ref},
-      start()
-  end.
-
-
-iter(ListFriends) ->
-  receive
-    {get_friends, Sender, _} ->
-      Sender ! {friends, make_ref(), ListFriends},
-      iter(ListFriends);
+      Sender ! {pong, Ref};
+    {get_friends, Sender, Nonce} ->
+      Sender ! {friends, make_ref(), Friends};
     {dead, Node, PID} ->
       exit(PID, kill),
-      iter(ListFriends)
-  %%      chiedo amici
-  end.
+      FriendsLess = Friends -- [Node],
+      spawn(test, getNewFriends, [FriendsLess, self()]),
+      start(FriendsLess)
+  end,
+  start(Friends).
 
-ext3Friends(ListFriends, List3Friends) ->  %%  when ListFriends=:= _:_
-  FilterListFriends = lists:filter(fun(Elem) -> not lists:member(Elem, List3Friends) end, ListFriends),
-  LfilterFriends=length(FilterListFriends),
+%% InitFriends è inserito per evitare di spawn nodi monitor già esistenti
+ext3Friends(ListFriends, MyListFriends, InitFriends) ->  %%  when ListFriends=:= _:_
+  FilterListFriends = lists:filter(fun(Elem) -> not lists:member(Elem, MyListFriends) end, ListFriends),
+  LfilterFriends = length(FilterListFriends),
   if
-    LfilterFriends< 1 -> lists:sublist(List3Friends, length(List3Friends) - 1);
+    LfilterFriends < 1 -> lists:sublist(MyListFriends, length(MyListFriends) - 1);
     true ->
       R = rand:uniform(length(FilterListFriends)),
-      Llist3Friends=length(List3Friends)-1,
+      Llist3Friends = length(MyListFriends) - 1,
       if
-        Llist3Friends < 3 -> ext3Friends(FilterListFriends, [lists:nth(R, FilterListFriends)] ++ List3Friends);
-        true -> lists:sublist(List3Friends, length(List3Friends) - 1)
+        Llist3Friends < 3 -> ext3Friends(FilterListFriends, [lists:nth(R, FilterListFriends)] ++ MyListFriends, InitFriends);
+        true -> lists:sublist(MyListFriends, length(MyListFriends) - 1) -- InitFriends
       end
   end.
+
+getNewFriends(Friends, PID) -> %%manca sleep quindi chiede iterativamente sempre
+%%  todo sleep prima di provare a parlare col prof
+%% todo  esaurire le richieste agli amici
+  NumFriends = length(Friends),
+%%  io:format("friend receive ~p ~p~n", [self(),NumFriends]),
+  if
+    NumFriends >= 3 -> exit(self(), kill);
+    true -> if
+              NumFriends == 0 ->%%chiamo il nodo prof
+                global:send(teacher_node, {get_friends, PID, make_ref()});
+              true ->
+                R = rand:uniform(NumFriends),
+                lists:nth(R, Friends) ! {get_friends, PID, make_ref()}
+            end
+  end.
+
 
 %% c(test). test:test().  test:start().
