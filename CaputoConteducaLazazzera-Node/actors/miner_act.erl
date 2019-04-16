20205f11da6e8049a4e47fccd4bb788569f5212b
antonioconte <antonio.conte05@live.it>
2019-04-16 15:56:48 +0200

    ask all get_previous

--- CaputoConteducaLazazzera-Node/actors/miner_act.erl
+++ CaputoConteducaLazazzera-Node/actors/miner_act.erl
@@ -15,17 +15,16 @@ compute(PidRoot,PidBlock) ->
         % OrderTrans = lists:sort(Transactions),
         % Nums = lists:flatmap(fun(X)-> {R, _}=X, returnLastNum(R) end, OrderTrans),
         % B = {Nums, ID_blocco_prev, Transactions, Solution},
-
         % Prof Version
         B = {make_ref(), ID_blocco_prev, Transactions, Solution},
         PidBlock ! {miningFinished, B},
         compute(PidRoot,PidBlock)
     end.
 
-returnLastNum(Ref) ->
-    [RefString] = io_lib:format("~p", [Ref]),
-    {match, L} = re:run(RefString, "#Ref<(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)>",[{capture, all_but_first, list}]),
-    lists:nth(length(L),L).
+% returnLastNum(Ref) ->
+    % [RefString] = io_lib:format("~p", [Ref]),
+    % {match, L} = re:run(RefString, "#Ref<(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)>",[{capture, all_but_first, list}]),
+    % lists:nth(length(L),L).
     
 
 
